6d6efb314ed12eb2e00013244986daad0229267f
kensunjaya <93558406+kensunjaya@users.noreply.github.com>
2022-08-07 11:23:31 +0700

    Update arduino code - motion tracking.ino

--- arduino code - motion tracking.ino
+++ arduino code - motion tracking.ino
@@ -1,17 +1,15 @@
 #include <Servo.h>
 
-#define ledPin 3
 #define buzzerPin 8
 #define laserPin 11
 
 Servo servo_x;
 Servo servo_y;
 
-static int degree_x, degree_y;
 int sensitivity_x = 22; //Lower value = more sensitive
 int sensitivity_y = 25; //Lower value = more sensitive
 
-static int x_axis, y_axis;
+static int degree_x, degree_y, x_axis, y_axis;
 static String b;
 
 
@@ -22,8 +20,8 @@ void setup() {
   servo_x.write(95);
   servo_y.write(30);
   digitalWrite(laserPin, HIGH);
-  // Clear the display
-  Serial.begin(2000000);
+
+  Serial.begin(2000000); //set the baudrate to 2000000
   Serial.setTimeout(2);
 }
  
@@ -35,22 +33,18 @@ void loop() {
   b = Serial.readString();
   x_axis = b.substring(0, b.indexOf(" ")).toInt();
   y_axis = b.substring(b.indexOf(" ")+1).toInt();
-  if ((x_axis + y_axis) != 0) {
-    
-    //Serial.println("x = " + String(x_axis) + " | y = " + String(y_axis)); --> causes the program to freeze
   
+  if ((x_axis + y_axis) != 0) {
  
     degree_x = x_axis/sensitivity_x;
     degree_y = y_axis/sensitivity_y;
 
-
     servo_x.write(52 + degree_x);
     servo_y.write(degree_y);
 
-    //Serial.print("x = " + String(degree_x) + " | ");
-    //Serial.println("y = " + String(degree_y));
   }
   else {
+    
     if (digitalRead(laserPin) == HIGH)
     digitalWrite(laserPin, LOW);
     else
